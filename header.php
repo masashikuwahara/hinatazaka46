e43f890f82492cebd308d36c5f43a08a79f6cb70
masashikuwahara <brucemclarenp4@gmail.com>
2023-11-25 04:18:08 +0900

    ヘッダー、メニュー更新

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <header>
-  <div class="header-logo">日向坂46応援サイト(仮)</div>
+  <div class="header-logo"><a href="index.php">日向坂46応援サイト(仮)</a></div>
   <div class="header-list">
     <ul>
-      <li class="header-list__item"><a href="about.php">このページについて</a></li>
       <li class="header-list__item"><a href="member.php">メンバー</a></li>
       <li class="header-list__item"><a href="discography.php">ディスコグラフィー</a></li>
+      <li class="header-list__item"><a href="sns.php">SNS</a></li>
     </ul>
   </div>
 
@@ -16,13 +16,12 @@
   <nav class="global-nav">
     <div class="global-nav__inner">
       <ul class="global-nav__list">
-        <li class="global-nav__item"><a href="about.php">このページについて</a></li>
         <li class="global-nav__item"><a href="member.php">メンバー</a></li>
         <li class="global-nav__item"><a href="discography.php">ディスコグラフィー</a></li>
+        <li class="global-nav__item"><a href="sns.php">SNS</a></li>
       </ul>
-      <div class="test">
-        test
-        test
+      <div class="sns">
+        <a href="https://twitter.com/hinatazaka46"><img src="img/logo-white.png" alt=""></a>
       </div>
      </div>
   </nav>
