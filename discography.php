909bd1bb3eb60b9dc7cff8eba4ee44f390eb043a
masashikuwahara <brucemclarenp4@gmail.com>
2023-11-26 23:52:36 +0900

    クラスを付与

--- discography.php
+++ discography.php
@@ -33,67 +33,68 @@
   <h1 class="title">DISCOGRAPHY</h1>
   <h2>SINGLE</h2>
   <div class="pagetop">TOP</div>
-  <table border="1" style="border-collapse: collapse">
-    <tr>
+  <table border="1" style="border-collapse: collapse" class="disco-list">
+    <tr class="disco-list">
       <th>枚</th>
       <th>アートワーク</th>
       <th>リリース日</th>
       <th>タイトル</th>
     </tr>
-    <tr>
+    <tr class="disco-list">
       <td>1</td>
       <td><img src="img/kyun.jpg" alt=""></td>
       <td>2019年3月27日</td>
       <td>キュン</td>
     </tr>
-    <tr>
+    <tr class="disco-list">
       <td>2</td>
       <td><img src="img/doremi.jpg" alt=""></td>
       <td>2019年7月17日</td>
       <td>ドレミソラシド</td>
     </tr>
+    <tr class="disco-list">
       <td>3</td>
       <td><img src="img/konsuki.jpg" alt=""></td>
       <td>2019年10月2日</td>
       <td>こんなに好きになっちゃっていいの?</td>
     </tr>
-    <tr>
+    <tr class="disco-list">
       <td>4</td>
       <td><img src="img/sonnakotonaiyo.jpg" alt=""></td>
       <td>2020年2月19日</td>
       <td>ソンナコトナイヨ</td>
     </tr>
-    <tr>
+    <tr class="disco-list">
       <td>5</td>
       <td><img src="img/kimishika.jpg" alt=""></td>
       <td>2021年5月26日</td>
       <td>君しか勝たん</td>
     </tr>
-    <tr>
+    <tr class="disco-list">
       <td>6</td>
       <td><img src="img/tteka.jpg" alt=""></td>
       <td>2021年10月27日</td>
       <td>ってか</td>
     </tr>
-    <tr>
+    <tr class="disco-list">
       <td>7</td>
       <td><img src="img/bokunannka.jpg" alt=""></td>
       <td>2022年6月1日</td>
       <td>僕なんか</td>
     </tr>
-    <tr>
+    <tr class="disco-list">
       <td>8</td>
       <td><img src="img/tsukihoshi.jpg" alt=""></td>
       <td>2022年10月26日</td>
       <td>月と星が踊るMidnight</td>
     </tr>
-    <tr>
+    <tr class="disco-list">
       <td>9</td>
       <td><img src="img/onechoice.jpg" alt=""></td>
       <td>2023年4月19日</td>
       <td>One choice</td>
     </tr>
-    <tr>
+    <tr class="disco-list">
       <td>10</td>
       <td><img src="img/amiready.jpg" alt=""></td>
       <td>2023年7月26日</td>
@@ -103,25 +104,25 @@
 
   <h2>ALBUM</h2>
   <table border="1" style="border-collapse: collapse">
-    <tr>
+    <tr class="disco-list">
       <th>枚</th>
       <th>アートワーク</th>
       <th>リリース日</th>
       <th>タイトル</th>
     </tr>
-    <tr>
+    <tr class="disco-list">
       <td>1</td>
       <td><img src="img/hashiridasu.jpg" alt=""></td>
       <td>2018年6月20日</td>
       <td>走り出す瞬間</td>
     </tr>
-    <tr>
+    <tr class="disco-list">
       <td>2</td>
       <td><img src="img/hinatazaka.jpg" alt=""></td>
       <td>2020年9月23日</td>
       <td>ひなたざか</td>
     </tr>
-    <tr>
+    <tr class="disco-list">
       <td>3</td>
       <td><img src="img/myakuutsu.jpg" alt=""></td>
       <td>2023年11月8日</td>
@@ -146,6 +147,10 @@
       pagetop_btn.style.opacity = "0"
     }
   }
+</script>
+<script src="https://unpkg.com/scrollreveal"></script>
+<script>
+ScrollReveal().reveal('.disco-list');
 </script>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="hamburger.js"></script>
--- member.php
+++ member.php
@@ -32,194 +32,194 @@
 <div class="wrapper">
   <h1 class="title">MEMBER</h1>
   <div class="pagetop">TOP</div>
-  <table border="1" style="border-collapse: collapse">
-    <tr>
+  <table border="1" style="border-collapse: collapse" class="member-list">
+    <tr class="member-list">
       <th></th>
       <th>名前</th>
       <th>生年月日</th>
       <th>期</th>
     </tr>
-    <tr>
-      <td><img src="img/ushio.jpg" alt=""></td>
+    <tr class="member-list">
+      <td class="member-list"><img src="img/ushio.jpg" alt=""></td>
       <td>潮紗理菜</td>
       <td>1997年12月26日</td>
       <td>一期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/shiho.jpg" alt=""></td>
       <td>加藤史帆</td>
       <td>1998年2月2日</td>
       <td>一期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/kyoko.jpg" alt=""></td>
       <td>齊藤京子</td>
       <td>1997年9月5日</td>
       <td>一期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/captain.jpg" alt=""></td>
       <td>佐々木久美</td>
       <td>1996年1月22日</td>
       <td>一期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/mirei.jpg" alt=""></td>
       <td>佐々木美玲</td>
       <td>1999年12月17日</td>
       <td>一期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/mana.jpg" alt=""></td>
       <td>高瀬愛奈</td>
       <td>1998年9月20日</td>
       <td>一期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/ayaka.jpg" alt=""></td>
       <td>高本彩花</td>
       <td>1998年11月2日</td>
       <td>一期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/mei.jpg" alt=""></td>
       <td>東村芽依</td>
       <td>1998年8月23日</td>
       <td>一期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/miku.jpg" alt=""></td>
       <td>金村美玖</td>
       <td>2002年9月10日</td>
       <td>二期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/hina.jpg" alt=""></td>
       <td>河田陽菜</td>
       <td>2001年7月23日</td>
       <td>二期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/nao.jpg" alt=""></td>
       <td>小坂菜緒</td>
       <td>2002年9月7日</td>
       <td>二期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/suzuka.jpg" alt=""></td>
       <td>富田鈴花</td>
       <td>2001年1月18日</td>
       <td>二期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/akari.jpg" alt=""></td>
       <td>丹生明里</td>
       <td>2001年2月15日</td>
       <td>二期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/hiyori.jpg" alt=""></td>
       <td>濱岸ひより</td>
       <td>2002年9月28日</td>
       <td>二期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/konoka.jpg" alt=""></td>
       <td>松田好花</td>
       <td>1999年4月27日</td>
       <td>二期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/hinano.jpg" alt=""></td>
       <td>上村ひなの</td>
       <td>2004年4月12日</td>
       <td>三期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/mikuni.jpg" alt=""></td>
       <td>髙橋未来虹</td>
       <td>2003年9月27日</td>
       <td>三期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/marie.jpg" alt=""></td>
       <td>森本茉莉</td>
       <td>2004年2月23日</td>
       <td>三期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/haruyo.jpg" alt=""></td>
       <td>山口陽世</td>
       <td>2004年2月23</td>
       <td>三期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/tamaki.jpg" alt=""></td>
       <td>石塚瑶季</td>
       <td>2004年8月6日</td>
       <td>四期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/honoka.jpg" alt=""></td>
       <td>岸帆夏</td>
       <td>2004年8月15日</td>
       <td>四期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/nanami.jpg" alt=""></td>
       <td>小西夏菜実</td>
       <td>2004年10月3日</td>
       <td>四期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/rio.jpg" alt=""></td>
       <td>清水理央</td>
       <td>2005年1月15日</td>
       <td>四期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/yoko.jpg" alt=""></td>
       <td>正源司陽子</td>
       <td>2007年2月14日</td>
       <td>四期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/kirari.jpg" alt=""></td>
       <td>竹内希来里</td>
       <td>2006年2月20日</td>
       <td>四期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/hiraho.jpg" alt=""></td>
       <td>平尾帆夏</td>
       <td>2003年7月31日</td>
       <td>四期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/mitsuki.jpg" alt=""></td>
       <td>平岡海月</td>
       <td>2002年4月9日</td>
       <td>四期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/kaho.jpg" alt=""></td>
       <td>藤嶌果歩</td>
       <td>2006年8月7日</td>
       <td>四期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/sumire.jpg" alt=""></td>
       <td>宮地すみれ</td>
       <td>2005年12月31日</td>
       <td>四期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/haruka.jpg" alt=""></td>
       <td>山下葉留花</td>
       <td>2003年5月20日</td>
       <td>四期</td>
     </tr>
-    <tr>
+    <tr class="member-list">
       <td><img src="img/rina.jpg" alt=""></td>
       <td>渡辺莉奈</td>
       <td>2009年2月7日</td>
@@ -243,6 +243,10 @@
       pagetop_btn.style.opacity = "0"
     }
   }
+</script>
+<script src="https://unpkg.com/scrollreveal"></script>
+<script>
+ScrollReveal().reveal('.member-list');
 </script>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="hamburger.js"></script>
