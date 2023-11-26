3555f65a4bd14cfe147b7295f2b9153c13660050
masashikuwahara <brucemclarenp4@gmail.com>
2023-11-26 23:52:56 +0900

    snsページ追加

--- sns.php
+++ sns.php
@@ -9,6 +9,23 @@
 <body>
 <?php require('header.php'); ?>
 <div class="wrapper">
+  <h1 class="title">SNS</h1>
+  <h2>X</h2>
+  <a href="https://twitter.com/hinatazaka46" target="_blank" rel="noopener noreferrer" class="sns-list">日向坂46公式X</a>
+  <h2>Instagram</h2>
+  <a href="https://instagram.com/ushiosarina8_8" target="_blank" rel="noopener noreferrer" class="sns-list">潮紗理菜</a><br>
+  <a href="https://www.instagram.com/katoshi.official/" target="_blank" rel="noopener noreferrer" class="sns-list">加藤史帆</a><br>
+  <a href="https://instagram.com/saitokyoko_official" target="_blank" rel="noopener noreferrer" class="sns-list">齊藤京子</a><br>
+  <a href="https://instagram.com/kumisasaki_" target="_blank" rel="noopener noreferrer" class="sns-list">佐々木久美</a><br>
+  <a href="https://instagram.com/mireisasaki_official" target="_blank" rel="noopener noreferrer" class="sns-list">佐々木美玲</a><br>
+  <a href="https://instagram.com/ayacheri._.official" target="_blank" rel="noopener noreferrer" class="sns-list">高本彩花</a><br>
+  <a href="https://instagram.com/mei.higashimura" target="_blank" rel="noopener noreferrer" class="sns-list">東村芽依</a><br>
+  <a href="https://instagram.com/miku_osushi" target="_blank" rel="noopener noreferrer" class="sns-list">金村美玖</a><br>
+  <a href="https://instagram.com/hina17_kawata" target="_blank" rel="noopener noreferrer" class="sns-list">河田陽菜</a><br>
+  <a href="https://instagram.com/suzy.tomita" target="_blank" rel="noopener noreferrer" class="sns-list">富田鈴花</a><br>
+  <a href="https://instagram.com/nibuchan_akari" target="_blank" rel="noopener noreferrer" class="sns-list">丹生明里</a><br>
+  <a href="https://instagram.com/hiyotan928_official" target="_blank" rel="noopener noreferrer" class="sns-list">濱岸ひより</a><br>
+  <a href="https://instagram.com/matsudakonoka.yahos" target="_blank" rel="noopener noreferrer" class="sns-list">松田好花</a><br>
 </div>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="hamburger.js"></script>
